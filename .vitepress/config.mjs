66ad92f8a92713161d4fbf77a23ea47f8f04ee97
郭林涛 <v-guolintao@sinochem.com>
2024-01-10 10:25:06 +0800

    ght

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,24 +2,26 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: "郭洪涛的个人站点",
+  description: "郭洪涛的个人站点",
   base: '/vp-blogs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      // { text: 'Examples', link: '/src/markdown-examples' },
+      // { text: 'Examples2', link: '/src/aaa/index' },
+      // { text: 'Examples3', link: '/src/bbb/index' }
     ],
 
     sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+      // {
+      //   text: 'Examples',
+      //   items: [
+      //     { text: 'Markdown Examples', link: '/markdown-examples' },
+      //     { text: 'Runtime API Examples', link: '/api-examples' }
+      //   ]
+      // }
     ],
 
     socialLinks: [
--- index.md
+++ index.md
@@ -3,16 +3,16 @@
 layout: home
 
 hero:
-  name: "test Project"
-  text: "A VitePress Site"
-  tagline: aaaaaaa
+  name: "Please call me 郭洪涛"
+  text: "安徽汴和律师事务所实习律师"
+  tagline: 前怀远法院聘用制书记员
   actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+    # - theme: brand
+    #   text: Markdown Examples
+    #   link: /markdown-examples
+    # - theme: alt
+    #   text: API Examples
+    #   link: /api-examples
 
 features:
   - title: Feature A
